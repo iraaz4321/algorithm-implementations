2e42f8d74ee49cba9087eec94b9a3058d91c3a87
iraaz4321 <69959972+iraaz4321@users.noreply.github.com>
2022-05-26 12:16:28 +0300

    added a comment

--- ciphers/Caesar/python/encrypt.py
+++ ciphers/Caesar/python/encrypt.py
@@ -11,6 +11,7 @@
 
 # Import string build in library to get alphabet
 import string
+# Import math library for floor function
 import math
 
 def caesar_encrypt(text: str, shift: int, alphabet: str = string.ascii_lowercase) -> str:
